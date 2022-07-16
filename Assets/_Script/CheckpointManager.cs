06ec683fe91bdb718a315bfe62d337448e24f053
phonghongs <phonghongs@gmail.com>
2022-07-16 16:18:45 +0700

    test source tree

--- Assets/_Script/CheckpointManager.cs
+++ Assets/_Script/CheckpointManager.cs
@@ -8,6 +8,7 @@ public class Checkpoints{
     private int numOfCP;
     private int[,] arrayCheckpointsInit;
     private SocketManager playerCreator;
+    int abc;
     public int getNumOfCP(){
         return numOfCP;
     }
